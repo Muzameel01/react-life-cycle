ccdd613ae028b0587cd0780accdd78af3f30daf9
Muzameel01 <mkmujameel@gmail.com>
2023-03-07 10:28:42 +0530

    feat: practice

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Component } from 'react';
 import { render } from '@testing-library/react';
 import { useState,useEffect } from 'react';
 import Profile from './Profile';
+import Avatar from './Avtar';
 
 function App() {
   // const[counter,setCounter]=useState(0)
@@ -33,7 +34,30 @@ function App() {
   {/* //       > */}
   {/* //         Learn React */} 
   {/* //       </a> */}
-    <Profile/>
+    {/* <Profile/> */}
+    <Avatar size={100
+  } person={{name:"XYZ", imageId:"1bX5QH6"}}/>  
+     <Avatar
+        size={100}
+        person={{ 
+          name: 'Katsuko Saruhashi', 
+          imageId: 'YfeOqp2'
+        }}
+      />
+      <Avatar
+        size={80}
+        person={{
+          name: 'Aklilu Lemma', 
+          imageId: 'OKS67lh'
+        }}
+      />
+      <Avatar
+        size={50}
+        person={{ 
+          name: 'Lin Lanying',
+          imageId: '1bX5QH6'
+        }}
+      />
       </header>
     </div>
   );
